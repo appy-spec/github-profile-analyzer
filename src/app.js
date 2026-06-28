5b0baef27d6fed281b311b236ec73b4ce9119517
appy-spec <rahulpatel120174@gmail.com>
2026-06-28 13:26:56 +0530

    do the setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes/profileRoutes");
+const errorHandler = require("./middleware/errorHandler");
+const notFound = require("./middleware/notFound");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api", routes);
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = app;
