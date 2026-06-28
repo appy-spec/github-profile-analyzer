75cd6ffc84d06f8e80fbc960d900cd8ee3b18207
appy-spec <rahulpatel120174@gmail.com>
2026-06-28 13:28:24 +0530

    add the schema

---
+++ database/schema.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE github_analyzer;
+USE github_analyzer;
+
+CREATE TABLE profiles(
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+username VARCHAR(100) UNIQUE NOT NULL,
+
+name VARCHAR(150),
+
+avatar_url TEXT,
+
+github_url TEXT,
+
+public_repos INT,
+
+followers INT,
+
+following INT,
+
+total_stars INT,
+
+total_forks INT,
+
+top_language VARCHAR(100),
+
+most_starred_repo VARCHAR(200),
+
+average_stars FLOAT,
+
+account_created DATE,
+
+analyzed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
